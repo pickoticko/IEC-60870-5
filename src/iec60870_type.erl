e3f5831008e74a8be8a065fe7edc0558b650364d
Alikhan <alikhantokenov@gmail.com>
2023-11-14 10:27:14 +0600

    Updates and fixes (timestamps)

--- src/iec60870_type.erl
+++ src/iec60870_type.erl
@@ -21,32 +21,32 @@
 
 %% Type 1. Single point information
 parse_information_element(?M_SP_NA_1, <<SIQ>>) ->
-  <<_Ignored:7, SPI:1>> = <<SIQ>>,
+  <<_Ignore:7, SPI:1>> = <<SIQ>>,
   #{value => SPI, siq => SIQ};
 
 %% Type 2. Single point information with time tag
 parse_information_element(?M_SP_TA_1, <<SIQ, Timestamp/binary>>) ->
-  <<_Ignored:7, SPI:1>> = <<SIQ>>,
+  <<_Ignore:7, SPI:1>> = <<SIQ>>,
   #{value => SPI, siq => SIQ, ts => parse_cp24(Timestamp)};
 
 %% Type 3. Double point information
 parse_information_element(?M_DP_NA_1, <<DIQ>>) ->
-  <<_Ignored:6, DPI:2>> = <<DIQ>>,
+  <<_Ignore:6, DPI:2>> = <<DIQ>>,
   #{value => DPI, diq => DIQ};
 
 %% Type 4. Double point information with time tag
 parse_information_element(?M_DP_TA_1, <<DIQ, Timestamp/binary>>) ->
-  <<_Ignored:6, DPI:2>> = <<DIQ>>,
+  <<_Ignore:6, DPI:2>> = <<DIQ>>,
   #{value => DPI, diq => DIQ, ts => parse_cp24(Timestamp)};
 
 %% Type 5. Step position information
 parse_information_element(?M_ST_NA_1, <<VTI, QDS>>) ->
-  <<_Ignored:1, Value:7/signed>> = <<VTI>>,
+  <<_Ignore:1, Value:7/signed>> = <<VTI>>,
   #{value => Value, vti => VTI, qds => QDS};
 
 %% Type 6. Step position information with time tag
 parse_information_element(?M_ST_TA_1, <<VTI, QDS, Timestamp/binary>>) ->
-  <<_Ignored:1, Value:7/signed>> = <<VTI>>,
+  <<_Ignore:1, Value:7/signed>> = <<VTI>>,
   #{value => Value, vti => VTI, qds => QDS, ts => parse_cp24(Timestamp)};
 
 %% Type 7. Bit string of 32 bit
@@ -83,17 +83,17 @@ parse_information_element(?M_ME_TC_1, <<Value:32/little-signed-float, QDS, Times
 
 %% Type 15. Integrated totals
 parse_information_element(?M_IT_NA_1, <<BCR:40>>) ->
-  <<Value:32/little-signed, _Ignored:8>> = <<BCR:40>>,
+  <<Value:32/little-signed, _Ignore:8>> = <<BCR:40>>,
   #{value => Value, bcr => BCR};
 
 %% Type 16. Integrated totals with time tag
 parse_information_element(?M_IT_TA_1, <<BCR:40, Timestamp/binary>>) ->
-  <<Value:32/little-signed, _Ignored:8>> = <<BCR:40>>,
+  <<Value:32/little-signed, _Ignore:8>> = <<BCR:40>>,
   #{value => Value, bcr => BCR, ts => parse_cp24(Timestamp)};
 
 %% Type 17. Protection equipment with time tag
 parse_information_element(?M_EP_TA_1, <<SEP, CP16:16, Timestamp/binary>>) ->
-  <<_Ignored:6, ES:2>> = <<SEP>>,
+  <<_Ignore:6, ES:2>> = <<SEP>>,
   #{value => ES, sep => SEP, duration => parse_cp16(CP16), ts => parse_cp24(Timestamp)};
 
 %% Type 18. Packed events of protection equipment with time tag
@@ -114,17 +114,17 @@ parse_information_element(?M_ME_ND_1, <<NVA:16/little-signed-float>>) ->
 
 %% Type 30. Single point information with time tag
 parse_information_element(?M_SP_TB_1, <<SIQ, Timestamp/binary>>) ->
-  <<_Ignored:7, SPI:1>> = <<SIQ>>,
+  <<_Ignore:7, SPI:1>> = <<SIQ>>,
   #{value => SPI, qds => SIQ, ts => parse_cp56(Timestamp)};
 
 %% Type 31. Double point information with time tag
 parse_information_element(?M_DP_TB_1, <<DIQ, Timestamp/binary>>) ->
-  <<_Ignored:6, DPI:2>> = <<DIQ>>,
+  <<_Ignore:6, DPI:2>> = <<DIQ>>,
   #{value => DPI, qds => DIQ, ts => parse_cp56(Timestamp)};
 
 %% Type 32. Step position information with time tag
 parse_information_element(?M_ST_TB_1, <<VTI, QDS, Timestamp/binary>>) ->
-  <<_Ignored:1, Value:7/signed>> = <<VTI>>,
+  <<_Ignore:1, Value:7/signed>> = <<VTI>>,
   #{value => Value, vti => VTI, qds => QDS, ts => parse_cp56(Timestamp)};
 
 %% Type 33. Bit string of 32 bit with time tag
@@ -145,17 +145,17 @@ parse_information_element(?M_ME_TF_1, <<Value:32/little-signed-float, QDS, Times
 
 %% Type 37. Integrated totals with time tag
 parse_information_element(?M_IT_TB_1, <<BCR:40, Timestamp/binary>>) ->
-  <<Value:32/little-signed, _Ignored:8>> = <<BCR:40>>,
+  <<Value:32/little-signed, _Ignore:8>> = <<BCR:40>>,
   #{value => Value, bcr => BCR, ts => parse_cp56(Timestamp)};
 
 %% Type 38. Event of protection equipment with time tag
 parse_information_element(?M_EP_TD_1, <<SEP, Interval:16/little-unsigned, Timestamp/binary>>) ->
-  <<_Ignored:6, ES:2>> = <<SEP>>,
+  <<_Ignore:6, ES:2>> = <<SEP>>,
   #{value => ES, sep => SEP, interval => Interval, ts => parse_cp56(Timestamp)};
 
 %% Type 70. End of initialization
 parse_information_element(?M_EI_NA_1, <<COI>>) ->
-  <<_Ignored:1, Value:7>> = <<COI>>,
+  <<_Ignore:1, Value:7>> = <<COI>>,
   #{value => Value, coi => COI};
 
 %% Type 100. Group request
@@ -192,14 +192,14 @@ create_information_element(?M_SP_TA_1, #{
   ts := Timestamp
 }) ->
   <<Rest:7, _Ignore:1>> = <<SIQ>>,
-  <<Rest:7, SPI:1, Timestamp/binary>>;
+  <<Rest:7, SPI:1, (get_cp24(Timestamp))/binary>>;
 
 %% Type 3. Double point information
 create_information_element(?M_DP_NA_1, #{
   value := DPI,
   diq := DIQ
 }) ->
-  <<Rest:6, _Ignored:2>> = <<DIQ>>,
+  <<Rest:6, _Ignore:2>> = <<DIQ>>,
   <<Rest:6, DPI:2>>;
 
 %% Type 4. Double point information with time tag
@@ -208,8 +208,8 @@ create_information_element(?M_DP_TA_1, #{
   diq := DIQ,
   ts := Timestamp
 }) ->
-  <<Rest:6, _Ignored:2>> = <<DIQ>>,
-  <<Rest:6, DPI:2, Timestamp/binary>>;
+  <<Rest:6, _Ignore:2>> = <<DIQ>>,
+  <<Rest:6, DPI:2, (get_cp24(Timestamp))/binary>>;
 
 %% Type 5. Step position information
 create_information_element(?M_ST_NA_1, #{
@@ -217,7 +217,7 @@ create_information_element(?M_ST_NA_1, #{
   vti := VTI,
   qds := QDS
 }) ->
-  <<State:1, _Ignored:7>> = <<VTI>>,
+  <<State:1, _Ignore:7>> = <<VTI>>,
   <<State:1, Value:7/signed, QDS>>;
 
 %% Type 6. Step position information with time tag
@@ -227,8 +227,8 @@ create_information_element(?M_ST_TA_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<State:1, _Ignored:7>> = <<VTI>>,
-  <<State:1, Value:7/signed, QDS, Timestamp/binary>>;
+  <<State:1, _Ignore:7>> = <<VTI>>,
+  <<State:1, Value:7/signed, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 7. Bit string of 32 bit
 create_information_element(?M_BO_NA_1, #{
@@ -243,7 +243,7 @@ create_information_element(?M_BO_TA_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<BSI:32/little-unsigned, QDS, Timestamp/binary>>;
+  <<BSI:32/little-unsigned, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 9. Measured value, normalized value
 create_information_element(?M_ME_NA_1, #{
@@ -258,7 +258,7 @@ create_information_element(?M_ME_TA_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<NVA:16/little-signed-float, QDS, Timestamp/binary>>;
+  <<NVA:16/little-signed-float, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 11. Measured value, scaled value
 create_information_element(?M_ME_NB_1, #{
@@ -273,7 +273,7 @@ create_information_element(?M_ME_TB_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<SVA:16/little-signed, QDS, Timestamp/binary>>;
+  <<SVA:16/little-signed, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 13. Measured value, short floating point
 create_information_element(?M_ME_NC_1, #{
@@ -288,7 +288,7 @@ create_information_element(?M_ME_TC_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<Value:32/little-signed-float, QDS, Timestamp/binary>>;
+  <<Value:32/little-signed-float, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 15. Integrated totals
 create_information_element(?M_IT_NA_1, #{
@@ -301,11 +301,11 @@ create_information_element(?M_IT_NA_1, #{
 %% Type 16. Integrated totals with time tag
 create_information_element(?M_IT_TA_1, #{
   value := Value,
-  cp8 := BCR,
+  bcr := BCR,
   ts := Timestamp
 }) ->
   <<_Ignore:32, Rest>> = <<BCR:40>>,
-  <<Value:32/little-signed, Rest, Timestamp/binary>>;
+  <<Value:32/little-signed, Rest, (get_cp24(Timestamp))/binary>>;
 
 %% Type 17. Protection equipment with time tag
 create_information_element(?M_EP_TA_1, #{
@@ -315,7 +315,7 @@ create_information_element(?M_EP_TA_1, #{
   ts := Timestamp
 }) ->
   <<Rest:6, _Ignore:2>> = <<SEP>>,
-  <<Rest:6, ES:2, Duration:16, Timestamp/binary>>;
+  <<Rest:6, ES:2, (get_cp16(Duration)):16/binary, (get_cp24(Timestamp))/binary>>;
 
 %% Type 18. Packed events of protection equipment with time tag
 create_information_element(?M_EP_TB_1, #{
@@ -324,16 +324,16 @@ create_information_element(?M_EP_TB_1, #{
   duration := Duration,
   ts := Timestamp
 }) ->
-  <<SPE, QDP, Duration:16, Timestamp/binary>>;
+  <<SPE, QDP, (get_cp16(Duration)):16/binary, (get_cp24(Timestamp))/binary>>;
 
 %% Type 19. Packed output circuit information of protection equipment with time tag
 create_information_element(?M_EP_TC_1, #{
   value := OCI,
   qdp := QDP,
-  duration := Timestamp16,
-  ts := Timestamp24
+  duration := Duration,
+  ts := Timestamp
 }) ->
-  <<OCI, QDP, Timestamp16:16, Timestamp24/binary>>;
+  <<OCI, QDP, (get_cp16(Duration)):16/binary, (get_cp24(Timestamp))/binary>>;
 
 %% Type 20. Packed single-point information with status change detection
 create_information_element(?M_PS_NA_1, #{
@@ -355,7 +355,7 @@ create_information_element(?M_SP_TB_1, #{
   ts := Timestamp
 }) ->
   <<Rest:7, _Ignore:1>> = <<SIQ>>,
-  <<Rest:7, SPI:1, Timestamp>>;
+  <<Rest:7, SPI:1, (get_cp56(Timestamp))/binary>>;
 
 %% Type 31. Double point information with time tag
 create_information_element(?M_DP_TB_1, #{
@@ -363,8 +363,8 @@ create_information_element(?M_DP_TB_1, #{
   qds := DIQ,
   ts := Timestamp
 }) ->
-  <<Rest:6, _Ignored:2>> = <<DIQ>>,
-  <<Rest:6, DPI:2, Timestamp/binary>>;
+  <<Rest:6, _Ignore:2>> = <<DIQ>>,
+  <<Rest:6, DPI:2, (get_cp56(Timestamp))/binary>>;
 
 %% Type 32. Step position information with time tag
 create_information_element(?M_ST_TB_1, #{
@@ -373,8 +373,8 @@ create_information_element(?M_ST_TB_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<State:1, _Ignored:7>> = <<VTI>>,
-  <<State:1, Value:7, QDS, Timestamp/binary>>;
+  <<State:1, _Ignore:7>> = <<VTI>>,
+  <<State:1, Value:7, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 33. Bit string of 32 bit with time tag
 create_information_element(?M_BO_TB_1, #{
@@ -382,7 +382,7 @@ create_information_element(?M_BO_TB_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<BSI:32/little-unsigned, QDS, Timestamp/binary>>;
+  <<BSI:32/little-unsigned, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 34. Measured value, normalized value with time tag
 create_information_element(?M_ME_TD_1, #{
@@ -390,7 +390,7 @@ create_information_element(?M_ME_TD_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<NVA:16/little-signed-float, QDS, Timestamp/binary>>;
+  <<NVA:16/little-signed-float, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 35. Measured value, scaled value with time tag
 create_information_element(?M_ME_TE_1, #{
@@ -398,7 +398,7 @@ create_information_element(?M_ME_TE_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<SVA:16/little-signed, QDS, Timestamp/binary>>;
+  <<SVA:16/little-signed, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 36. Measured value, short floating point value with time tag
 create_information_element(?M_ME_TF_1, #{
@@ -406,7 +406,7 @@ create_information_element(?M_ME_TF_1, #{
   qds := QDS,
   ts := Timestamp
 }) ->
-  <<Value:32/little-signed-float, QDS, Timestamp/binary>>;
+  <<Value:32/little-signed-float, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 37. Integrated totals with time tag
 create_information_element(?M_IT_TB_1, #{
@@ -415,15 +415,17 @@ create_information_element(?M_IT_TB_1, #{
   ts := Timestamp
 }) ->
   <<_Ignore:32, Rest:8>> = <<BCR:40>>,
-  <<Value:32/little-signed, Rest:8, Timestamp/binary>>;
+  <<Value:32/little-signed, Rest:8, (get_cp56(Timestamp))/binary>>;
 
 %% Type 38. Event of protection equipment with time tag
 create_information_element(?M_EP_TD_1, #{
-  value := SEP,
+  value := ES,
+  sep := SEP,
   interval := Interval,
   ts := Timestamp
 }) ->
-  <<SEP, Interval:16/little-unsigned, Timestamp/binary>>;
+  <<Rest:6, _Ignore:2>> = <<SEP>>,
+  <<Rest:6, ES:2, Interval:16/little-unsigned, (get_cp56(Timestamp))/binary>>;
 
 %% Type 70. End of initialization
 create_information_element(?M_EI_NA_1, #{
@@ -498,6 +500,11 @@ parse_cp56(CP) ->
       none
   end.
 
+get_cp16(undefined) -> get_cp16(0);
+get_cp16(Value) ->
+  <<Value/unsigned-integer>>.
+
+get_cp56(undefined) -> get_cp56(0);
 get_cp56(Value) ->
   try
     Gs = (Value + 62167219200000) div 1000,
@@ -506,6 +513,19 @@ get_cp56(Value) ->
     WD = calendar:day_of_the_week(Y, MN, D),
     YCP56 = Y - 2000,
     <<Ms:16/little-integer, 16#00:2, Min:6/integer, 16#00:3, H:5/integer, WD:3/integer, D:5/integer, 16#00:4, MN:4/integer, 16#00:1, YCP56:7/integer>>
+  catch
+    _:Error ->
+      ?LOGERROR("Timestamp error: ~p. Timestamp: ~p", [Value, Error]),
+      undefined
+  end.
+
+get_cp24(undefined) -> get_cp24(0);
+get_cp24(Value) ->
+  try
+    Gs = (Value + 62167219200000) div 1000,
+    {{_, _, _}, {H, Minutes, S}} = calendar:gregorian_seconds_to_datetime(Gs),
+    Millis = S * 1000 + (Value + 62167219200000) - Gs * 1000,
+    <<Millis:16/little-integer, 16#00:2, Minutes:6/integer, 16#00:3, H:5/integer>>
   catch
     _:Error ->
       ?LOGERROR("Timestamp error: ~p. Timestamp: ~p", [Value, Error]),

eda5d28eafbba15350314dd3db9f9fde7c958a18
Alikhan <alikhantokenov@gmail.com>
2023-11-24 04:54:02 +0600

    Refactoring

--- src/iec60870_balanced_server.erl
+++ src/iec60870_balanced_server.erl
@@ -1,57 +1,59 @@
-
 -module(iec60870_balanced_server).
 
 -include("iec60870.hrl").
 -include("ft12.hrl").
 
-%% API
 -export([
   start/2,
   stop/1
 ]).
 
+%% +--------------------------------------------------------------+
+%% |                             API                              |
+%% +--------------------------------------------------------------+
 
 start(Root, Options) ->
-  PID = spawn( fun() -> init(Root, Options) end),
+  PID = spawn(fun() -> init(Root, Options) end),
   receive
     {ready, PID} -> PID;
     {'EXIT', PID, Reason} -> throw(Reason)
   end.
 
-stop( PID )->
-  exit( PID, shutdown ).
+stop(PID) ->
+  exit(PID, shutdown).
+
+%% +--------------------------------------------------------------+
+%% |                     Internal  functions                      |
+%% +--------------------------------------------------------------+
 
 init(Root, Options)->
-
   process_flag(trap_exit, true),
+  Port = iec60870_balanced:start(_Direction = ?FROM_B_TO_A, Options),
+  Root ! {ready, self()},
+  wait_connection(Root, Port, Options).
 
-  Port = iec60870_balanced:start(_Dir=0, Options ),
-  Root ! { ready, self() },
-
-  wait_connection( Root, Port, Options ).
-
-wait_connection( Root, Port, Options )->
+wait_connection(Root, Port, Options)->
   receive
-    { connected, Port } ->
-      case iec60870_server:start_connection(Root, {?MODULE,self()}, Port ) of
+    {connected, Port} ->
+      case iec60870_server:start_connection(Root, {?MODULE, self()}, Port) of
         {ok, Connection} ->
-          Port ! { connection, self(), Connection },
-          wait_connection( Root, Port, Options );
-        error->
-          unlink( Port ),
+          Port ! {connection, self(), Connection},
+          wait_connection(Root, Port, Options);
+        error ->
+          unlink(Port),
           exit(Port, shutdown),
-          NewPort = iec60870_balanced:start(_Dir = 0, Options ),
-          wait_connection( Root, NewPort, Options )
+          NewPort = iec60870_balanced:start(_Direction = ?FROM_B_TO_A, Options),
+          wait_connection(Root, NewPort, Options)
       end;
-    {'EXIT', Port, Reason}->
+    {'EXIT', Port, Reason} ->
       case Reason of
-        connect_error->
+        connect_error ->
           ?LOGDEBUG("wait connection");
-        _->
+        _ ->
           ?LOGWARNING("port exit reason: ~p",[Reason])
       end,
-      NewPort = iec60870_balanced:start(_Dir =0, Options ),
-      wait_connection( Root, NewPort, Options );
-    {'EXIT', Root, Reason}->
+      NewPort = iec60870_balanced:start(_Direction = ?FROM_B_TO_A, Options),
+      wait_connection(Root, NewPort, Options);
+    {'EXIT', Root, Reason} ->
       exit(Port, Reason)
   end.

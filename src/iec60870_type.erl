d6c406ae4b849753d341cf64f31edd74433061c8
Alikhan <alikhantokenov@gmail.com>
2023-12-30 19:34:04 +0600

    Formatting

--- src/iec60870_type.erl
+++ src/iec60870_type.erl
@@ -244,52 +244,40 @@ parse_information_element(Type, Value) ->
 %% +--------------------------------------------------------------+
 
 %% Type 1. Single point information
-create_information_element(?M_SP_NA_1, #{
-  value := SPI
-} = Object) ->
+create_information_element(?M_SP_NA_1, #{value := SPI} = Object) ->
   SIQ = maps:get(siq, Object, 0),
   <<Rest:7, _Ignore:1>> = <<SIQ>>,
   <<Rest:7, (round(SPI)):1>>;
 
 %% Type 2. Single point information with time tag
-create_information_element(?M_SP_TA_1, #{
-  value := SPI
-} = Object) ->
+create_information_element(?M_SP_TA_1, #{value := SPI} = Object) ->
   SIQ = maps:get(siq, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:7, _Ignore:1>> = <<SIQ>>,
   <<Rest:7, (round(SPI)):1, (get_cp24(Timestamp))/binary>>;
 
 %% Type 3. Double point information
-create_information_element(?M_DP_NA_1, #{
-  value := DPI
-} = Object) ->
+create_information_element(?M_DP_NA_1, #{value := DPI} = Object) ->
   DIQ = maps:get(diq, Object, 0),
   <<Rest:6, _Ignore:2>> = <<DIQ>>,
   <<Rest:6, (round(DPI)):2>>;
 
 %% Type 4. Double point information with time tag
-create_information_element(?M_DP_TA_1, #{
-  value := DPI
-} = Object) ->
+create_information_element(?M_DP_TA_1, #{value := DPI} = Object) ->
   DIQ = maps:get(diq, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:6, _Ignore:2>> = <<DIQ>>,
   <<Rest:6, (round(DPI)):2, (get_cp24(Timestamp))/binary>>;
 
 %% Type 5. Step position information
-create_information_element(?M_ST_NA_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_ST_NA_1, #{value := Value} = Object) ->
   VTI = maps:get(vti, Object, 0),
   QDS = maps:get(qds, Object, 0),
   <<State:1, _Ignore:7>> = <<VTI>>,
   <<State:1, (round(Value)):7/signed, QDS>>;
 
 %% Type 6. Step position information with time tag
-create_information_element(?M_ST_TA_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_ST_TA_1, #{value := Value} = Object) ->
   VTI = maps:get(vti, Object, 0),
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
@@ -297,86 +285,64 @@ create_information_element(?M_ST_TA_1, #{
   <<State:1, (round(Value)):7/signed, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 7. Bit string of 32 bit
-create_information_element(?M_BO_NA_1, #{
-  value := BSI
-} = Object) ->
+create_information_element(?M_BO_NA_1, #{value := BSI} = Object) ->
   QDS = maps:get(qds, Object, 0),
   <<(round(BSI)):32/little-unsigned, QDS>>;
 
 %% Type 8. Bit string of 32 bit with time tag
-create_information_element(?M_BO_TA_1, #{
-  value := BSI
-} = Object) ->
+create_information_element(?M_BO_TA_1, #{value := BSI} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(BSI)):32/little-unsigned, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 9. Measured value, normalized value
-create_information_element(?M_ME_NA_1, #{
-  value := NVA
-} = Object) ->
+create_information_element(?M_ME_NA_1, #{value := NVA} = Object) ->
   QDS = maps:get(qds, Object, 0),
   <<(round(get_nva(NVA))):16/little-signed, QDS>>;
 
 %% Type 10. Measured value, normalized value with time tag
-create_information_element(?M_ME_TA_1, #{
-  value := NVA
-} = Object) ->
+create_information_element(?M_ME_TA_1, #{value := NVA} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(get_nva(NVA))):16/little-signed, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 11. Measured value, scaled value
-create_information_element(?M_ME_NB_1, #{
-  value := SVA
-} = Object) ->
+create_information_element(?M_ME_NB_1, #{value := SVA} = Object) ->
   QDS = maps:get(qds, Object, 0),
   <<(round(SVA)):16/little-signed, QDS>>;
 
 %% Type 12. Measured value, scaled value with time tag
-create_information_element(?M_ME_TB_1, #{
-  value := SVA
-} = Object) ->
+create_information_element(?M_ME_TB_1, #{value := SVA} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(SVA)):16/little-signed, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 13. Measured value, short floating point
-create_information_element(?M_ME_NC_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_ME_NC_1, #{value := Value} = Object) ->
   QDS = maps:get(qds, Object, 0),
   <<Value:32/little-signed-float, QDS>>;
 
 %% Type 14. Measured value, short floating point with time tag
-create_information_element(?M_ME_TC_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_ME_TC_1, #{value := Value} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Value:32/little-signed-float, QDS, (get_cp24(Timestamp))/binary>>;
 
 %% Type 15. Integrated totals
-create_information_element(?M_IT_NA_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_IT_NA_1, #{value := Value} = Object) ->
   BCR = maps:get(bcr, Object, 0),
   <<_Ignore:32, Rest>> = <<BCR:40>>,
   <<(round(Value)):32/little-signed, Rest>>;
 
 %% Type 16. Integrated totals with time tag
-create_information_element(?M_IT_TA_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_IT_TA_1, #{value := Value} = Object) ->
   BCR = maps:get(bcr, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<_Ignore:32, Rest>> = <<BCR:40>>,
   <<(round(Value)):32/little-signed, Rest, (get_cp24(Timestamp))/binary>>;
 
 %% Type 17. Protection equipment with time tag
-create_information_element(?M_EP_TA_1, #{
-  value := ES
-} = Object) ->
+create_information_element(?M_EP_TA_1, #{value := ES} = Object) ->
   SEP = maps:get(sep, Object, 0),
   Duration = maps:get(duration, Object, undefined),
   Timestamp = maps:get(ts, Object, undefined),
@@ -384,58 +350,44 @@ create_information_element(?M_EP_TA_1, #{
   <<Rest:6, (round(ES)):2, (get_cp16(Duration)):16, (get_cp24(Timestamp))/binary>>;
 
 %% Type 18. Packed events of protection equipment with time tag
-create_information_element(?M_EP_TB_1, #{
-  value := SPE
-} = Object) ->
+create_information_element(?M_EP_TB_1, #{value := SPE} = Object) ->
   QDP = maps:get(qdp, Object, 0),
   Duration = maps:get(duration, Object, undefined),
   Timestamp = maps:get(ts, Object, undefined),
   <<SPE, QDP, (get_cp16(Duration)):16, (get_cp24(Timestamp))/binary>>;
 
 %% Type 19. Packed output circuit information of protection equipment with time tag
-create_information_element(?M_EP_TC_1, #{
-  value := OCI
-} = Object) ->
+create_information_element(?M_EP_TC_1, #{value := OCI} = Object) ->
   QDP = maps:get(qdp, Object, 0),
   Duration = maps:get(duration, Object, undefined),
   Timestamp = maps:get(ts, Object, undefined),
   <<OCI, QDP, (get_cp16(Duration)):16, (get_cp24(Timestamp))/binary>>;
 
 %% Type 20. Packed single-point information with status change detection
-create_information_element(?M_PS_NA_1, #{
-  value := SCD
-} = Object) ->
+create_information_element(?M_PS_NA_1, #{value := SCD} = Object) ->
   QDS = maps:get(qds, Object, 0),
   <<(round(SCD)):32/little, QDS>>;
 
 %% Type 21. Measured value, normalized value without QDS
-create_information_element(?M_ME_ND_1, #{
-  value := NVA
-}) ->
+create_information_element(?M_ME_ND_1, #{value := NVA}) ->
   <<(round(get_nva(NVA))):16/little-signed>>;
 
 %% Type 30. Single point information with time tag
-create_information_element(?M_SP_TB_1, #{
-  value := SPI
-} = Object) ->
+create_information_element(?M_SP_TB_1, #{value := SPI} = Object) ->
   SIQ = maps:get(siq, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:7, _Ignore:1>> = <<SIQ>>,
   <<Rest:7, (round(SPI)):1, (get_cp56(Timestamp))/binary>>;
 
 %% Type 31. Double point information with time tag
-create_information_element(?M_DP_TB_1, #{
-  value := DPI
-} = Object) ->
+create_information_element(?M_DP_TB_1, #{value := DPI} = Object) ->
   DIQ = maps:get(diq, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:6, _Ignore:2>> = <<DIQ>>,
   <<Rest:6, (round(DPI)):2, (get_cp56(Timestamp))/binary>>;
 
 %% Type 32. Step position information with time tag
-create_information_element(?M_ST_TB_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_ST_TB_1, #{value := Value} = Object) ->
   VTI = maps:get(vti, Object, 0),
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
@@ -443,50 +395,38 @@ create_information_element(?M_ST_TB_1, #{
   <<State:1, (round(Value)):7, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 33. Bit string of 32 bit with time tag
-create_information_element(?M_BO_TB_1, #{
-  value := BSI
-} = Object) ->
+create_information_element(?M_BO_TB_1, #{value := BSI} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(BSI)):32/little-unsigned, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 34. Measured value, normalized value with time tag
-create_information_element(?M_ME_TD_1, #{
-  value := NVA
-} = Object) ->
+create_information_element(?M_ME_TD_1, #{value := NVA} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(get_nva(NVA))):16/little-signed, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 35. Measured value, scaled value with time tag
-create_information_element(?M_ME_TE_1, #{
-  value := SVA
-} = Object) ->
+create_information_element(?M_ME_TE_1, #{value := SVA} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(SVA)):16/little-signed, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 36. Measured value, short floating point value with time tag
-create_information_element(?M_ME_TF_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_ME_TF_1, #{value := Value} = Object) ->
   QDS = maps:get(qds, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Value:32/little-signed-float, QDS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 37. Integrated totals with time tag
-create_information_element(?M_IT_TB_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_IT_TB_1, #{value := Value} = Object) ->
   BCR = maps:get(bcr, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<_Ignore:32, Rest:8>> = <<BCR:40>>,
   <<(round(Value)):32/little-signed, Rest:8, (get_cp56(Timestamp))/binary>>;
 
 %% Type 38. Event of protection equipment with time tag
-create_information_element(?M_EP_TD_1, #{
-  value := ES
-} = Object) ->
+create_information_element(?M_EP_TD_1, #{value := ES} = Object) ->
   SEP = maps:get(sep, Object, 0),
   Interval = maps:get(interval, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
@@ -494,118 +434,88 @@ create_information_element(?M_EP_TD_1, #{
   <<Rest:6, (round(ES)):2, Interval:16/little-unsigned, (get_cp56(Timestamp))/binary>>;
 
 %% Type 45: Single command
-create_information_element(?C_SC_NA_1, #{
-  value := SCS
-} = Object) ->
+create_information_element(?C_SC_NA_1, #{value := SCS} = Object) ->
   SCO = maps:get(sco, Object, 0),
   <<Rest:7, _Ignore:1>> = <<SCO>>,
   <<Rest:7, (round(SCS)):1>>;
 
 %% Type 46: Double command
-create_information_element(?C_DC_NA_1, #{
-  value := DCS
-} = Object) ->
+create_information_element(?C_DC_NA_1, #{value := DCS} = Object) ->
   DCO = maps:get(dco, Object, 0),
   <<Rest:6, _Ignore:2>> = <<DCO>>,
   <<Rest:6, (round(DCS)):2>>;
 
 %% Type 47: Regulating step command
-create_information_element(?C_RC_NA_1, #{
-  value := RCS
-} = Object) ->
+create_information_element(?C_RC_NA_1, #{value := RCS} = Object) ->
   RCO = maps:get(rco, Object, 0),
   <<Rest:6, _Ignore:2>> = <<RCO>>,
   <<Rest:6, (round(RCS)):2>>;
 
 %% Type 48: Set point command, normalized value
-create_information_element(?C_SE_NA_1, #{
-  value := NVA
-} = Object) ->
+create_information_element(?C_SE_NA_1, #{value := NVA} = Object) ->
   QOS = maps:get(qos, Object, 0),
   <<(round(get_nva(NVA))):16/little-signed, QOS>>;
 
 %% Type 49: Set point command, scaled value
-create_information_element(?C_SE_NB_1, #{
-  value := SVA
-} = Object) ->
+create_information_element(?C_SE_NB_1, #{value := SVA} = Object) ->
   QOS = maps:get(qos, Object, 0),
   <<(round(SVA)):16/little-signed, QOS>>;
 
 %% Type 50: Set point command, short floating point value
-create_information_element(?C_SE_NC_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?C_SE_NC_1, #{value := Value} = Object) ->
   QOS = maps:get(qos, Object, 0),
   <<Value:32/little-signed-float, QOS>>;
 
 %% Type 51: Bit string 32 bit
-create_information_element(?C_BO_NA_1, #{
-  value := BSI
-}) ->
+create_information_element(?C_BO_NA_1, #{value := BSI}) ->
   <<(round(BSI)):32/little-unsigned>>;
 
 %% Type 58: Single command with time tag
-create_information_element(?C_SC_TA_1, #{
-  value := SCS
-} = Object) ->
+create_information_element(?C_SC_TA_1, #{value := SCS} = Object) ->
   SCO = maps:get(sco, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:7, _Ignore:1>> = <<SCO>>,
   <<Rest:7, (round(SCS)):1, (get_cp56(Timestamp))/binary>>;
 
 %% Type 59: Double command with time tag
-create_information_element(?C_DC_TA_1, #{
-  value := DCS
-} = Object) ->
+create_information_element(?C_DC_TA_1, #{value := DCS} = Object) ->
   DCO = maps:get(dco, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:6, _Ignore:2>> = <<DCO>>,
   <<Rest:6, (round(DCS)):2, (get_cp56(Timestamp))/binary>>;
 
 %% Type 60: Regulating step command with time tag
-create_information_element(?C_RC_TA_1, #{
-  value := RCS
-} = Object) ->
+create_information_element(?C_RC_TA_1, #{value := RCS} = Object) ->
   RCO = maps:get(rco, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Rest:6, _Ignore:2>> = <<RCO>>,
   <<Rest:6, (round(RCS)):2, (get_cp56(Timestamp))/binary>>;
 
 %% Type 61: Set point command, normalized value with time tag
-create_information_element(?C_SE_TA_1, #{
-  value := NVA
-} = Object) ->
+create_information_element(?C_SE_TA_1, #{value := NVA} = Object) ->
   QOS = maps:get(qos, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(get_nva(NVA))):16/little-signed, QOS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 62: Set point command, scaled value with time tag
-create_information_element(?C_SE_TB_1, #{
-  value := SVA
-} = Object) ->
+create_information_element(?C_SE_TB_1, #{value := SVA} = Object) ->
   QOS = maps:get(qos, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(SVA)):16/little-signed, QOS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 63: Set point command, short floating point value with time tag
-create_information_element(?C_SE_TC_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?C_SE_TC_1, #{value := Value} = Object) ->
   QOS = maps:get(qos, Object, 0),
   Timestamp = maps:get(ts, Object, undefined),
   <<Value:32/little-signed-float, QOS, (get_cp56(Timestamp))/binary>>;
 
 %% Type 64: Bit string 32 bit with time tag
-create_information_element(?C_BO_TA_1, #{
-  value := BSI
-} = Object) ->
+create_information_element(?C_BO_TA_1, #{value := BSI} = Object) ->
   Timestamp = maps:get(ts, Object, undefined),
   <<(round(BSI)):32/little-unsigned, (get_cp56(Timestamp))/binary>>;
 
 %% Type 70. End of initialization
-create_information_element(?M_EI_NA_1, #{
-  value := Value
-} = Object) ->
+create_information_element(?M_EI_NA_1, #{value := Value} = Object) ->
   COI = maps:get(coi, Object, 0),
   <<COI:1, _Ignore:7>> = <<COI>>,
   <<COI:1, (round(Value)):7>>;

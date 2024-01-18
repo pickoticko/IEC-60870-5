0a67569003fc1c0b421a9b2d73f3488a279c6de9
Alikhan <alikhantokenov@gmail.com>
2024-01-18 10:40:31 +0600

    Common address check function

--- src/iec60870_asdu.erl
+++ src/iec60870_asdu.erl
@@ -44,12 +44,7 @@ parse(ASDU, #{
     org  := ORG,
     coa  := COA
   } = DUI,
-  if
-    COA =:= InCOA ->
-      ignore;
-    true ->
-      throw({error, invalid_coa_received})
-  end,
+  check_common_address(InCOA, COA),
   ParsedObjects =
     [begin
        {Address, iec60870_type:parse_information_element(Type, Object)}
@@ -177,3 +172,8 @@ split(DataObjects, MaxSize) when length(DataObjects) > MaxSize ->
   [Head | split(Tail, MaxSize)];
 split(DataObjects, _MaxSize)->
   [DataObjects].
+
+check_common_address(COA, ReceivedCOA) when COA =:= ReceivedCOA ->
+  ok;
+check_common_address(_, _) ->
+  throw({error, invalid_coa_received}).

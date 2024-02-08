cbb33431857c8fd18be123e4e72a746a871249cd
hottodoku <alikhantokenov@gmail.com>
2024-02-08 17:10:42 +0600

    Request status of link handling fix

--- src/iec60870_unbalanced_server.erl
+++ src/iec60870_unbalanced_server.erl
@@ -163,14 +163,24 @@ handle_request(?ACCESS_DEMAND, _UserData, #data{
     })
   };
 
-handle_request(?REQUEST_STATUS_LINK, UserData, #data{
+handle_request(?REQUEST_STATUS_LINK, _UserData, #data{
+  root = Root,
   port = Port,
   address = Address,
   connection = Connection
 } = Data) ->
   if
     is_pid(Connection) ->
+      ?LOGINFO("server on port ~p received request for status link... restarting the connection", [Port]),
+      exit(Connection, shutdown);
+    true ->
+      ignore
+  end,
+  case iec60870_server:start_connection(Root, {?MODULE, self()}, self()) of
+    {ok, NewConnection} ->
+      erlang:monitor(process, NewConnection),
       Data#data{
+        connection = NewConnection,
         sent_frame = send_response(Port, #frame{
           address = Address,
           control_field = #control_field_response{
@@ -181,9 +191,19 @@ handle_request(?REQUEST_STATUS_LINK, UserData, #data{
           }
         })
       };
-    true ->
-      ?LOGWARNING("request status of link received on no longer alive connection ~p", [UserData]),
-      Data
+    error ->
+      Data#data{
+        connection = undefined,
+        sent_frame = send_response(Port, #frame{
+          address = Address,
+          control_field = #control_field_response{
+            direction = 0,
+            acd = 0,
+            dfc = 0,
+            function_code = ?ERR_NOT_FUNCTIONING
+          }
+        })
+      }
   end;
 
 handle_request(?REQUEST_DATA_CLASS_1, _UserData, #data{

4ecde82e29b593469193eeae113ed5433e467e70
hottodoku <alikhantokenov@gmail.com>
2024-04-17 10:27:53 +0500

    Debug messages removed

--- src/iec60870_server.erl
+++ src/iec60870_server.erl
@@ -161,12 +161,10 @@ update_value(#?MODULE{name = Name, storage = Storage}, ID, InValue) ->
       }
     end,
   Value = maps:merge(OldValue, InValue),
-  case is_equal( Value, OldValue ) of
+  case is_equal(Value, OldValue) of
     true ->
-      ?LOGINFO("DEBUG: ~p is equal",[ID]),
       ok;
-    _->
-      ?LOGINFO("DEBUG: ~p sporadic update",[ID]),
+    _ ->
       %% Value must contain 'value' parameter
       check_value(Value),
       ets:insert(Storage, {ID, Value}),
@@ -176,15 +174,14 @@ update_value(#?MODULE{name = Name, storage = Storage}, ID, InValue) ->
       esubscribe:notify(Name, ID, Value)
   end.
 
-is_equal( Value, #{ type:=Type, value:=_Value }=PrevValue )->
+is_equal(Value, #{type := Type, value := _Value} = PrevValue) ->
   try
-    iec60870_type:create_information_element( Type, Value )
-      =:=
-    iec60870_type:create_information_element( Type, PrevValue )
+    iec60870_type:create_information_element(Type, Value) =:=
+      iec60870_type:create_information_element(Type, PrevValue)
   catch
-    _:_->false
+    _:_ -> false
   end;
-is_equal( _Value, _PrevValue )->
+is_equal(_Value, _PrevValue) ->
   false.
 
 %% +--------------------------------------------------------------+

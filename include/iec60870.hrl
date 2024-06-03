b3255516ebd0b2a1a75cc4cb1a230ae10f2ef545
hottodoku <alikhantokenov@gmail.com>
2024-06-03 17:02:26 +0500

    Added logger configurations and dependency

--- include/iec60870.hrl
+++ include/iec60870.hrl
@@ -7,14 +7,16 @@
 -ifndef(iec60870).
 -define(iec60870, 1).
 
--define(LOGERROR(Text), logger:error(Text)).
--define(LOGERROR(Text,Params), logger:error( Text, Params)).
--define(LOGWARNING(Text), logger:warning(Text)).
+-include_lib("kernel/include/logger.hrl").
+
+-define(LOGERROR(Text),          logger:error(Text)).
+-define(LOGERROR(Text,Params),   logger:error( Text, Params)).
+-define(LOGWARNING(Text),        logger:warning(Text)).
 -define(LOGWARNING(Text,Params), logger:warning(Text, Params)).
--define(LOGINFO(Text), logger:info(Text)).
--define(LOGINFO(Text,Params), logger:info(Text, Params)).
--define(LOGDEBUG(Text), logger:debug(Text)).
--define(LOGDEBUG(Text,Params), logger:debug(Text,Params)).
+-define(LOGINFO(Text),           logger:info(Text)).
+-define(LOGINFO(Text,Params),    logger:info(Text, Params)).
+-define(LOGDEBUG(Text),          logger:debug(Text)).
+-define(LOGDEBUG(Text,Params),   logger:debug(Text,Params)).
 
 -define(DATA(Connection, Data), {data, Connection, Data}).
 -define(OBJECT(Type, COT, IOA, Value), {object, Type, COT, IOA, Value}).

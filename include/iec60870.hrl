bea13adf7dec41b24b0820b84564bd1805f0c66b
roman <vzroman@gmail.com>
2024-12-19 18:10:23 +0500

    move from lager to logger

--- include/iec60870.hrl
+++ include/iec60870.hrl
@@ -7,17 +7,37 @@
 -ifndef(iec60870).
 -define(iec60870, 1).
 
--include_lib("kernel/include/logger.hrl").
+%%-------------------------------------------------------------------------------
+%% LOGGING
+%%-------------------------------------------------------------------------------
+-ifndef(TEST).
 
--define(SELF, pid_to_list(self()) ++ ": ").
--define(LOGERROR(Text),           ?LOG_ERROR(?SELF ++ Text)).
--define(LOGERROR(Text, Params),   ?LOG_ERROR(?SELF ++ Text, Params)).
--define(LOGWARNING(Text),         ?LOG_WARNING(?SELF ++ Text)).
--define(LOGWARNING(Text, Params), ?LOG_WARNING(?SELF ++ Text, Params)).
--define(LOGINFO(Text),            ?LOG_INFO(?SELF ++ Text)).
--define(LOGINFO(Text, Params),    ?LOG_INFO(?SELF ++ Text, Params)).
--define(LOGDEBUG(Text),           ?LOG_DEBUG(?SELF ++ Text)).
--define(LOGDEBUG(Text, Params),   ?LOG_DEBUG(?SELF ++ Text, Params)).
+-define(MFA_METADATA, #{
+  mfa => {?MODULE, ?FUNCTION_NAME, ?FUNCTION_ARITY},
+  line => ?LINE
+}).
+
+-define(LOGERROR(Text),          logger:error(Text, [], ?MFA_METADATA)).
+-define(LOGERROR(Text,Params),   logger:error(Text, Params, ?MFA_METADATA)).
+-define(LOGWARNING(Text),        logger:warning(Text, [], ?MFA_METADATA)).
+-define(LOGWARNING(Text,Params), logger:warning(Text, Params, ?MFA_METADATA)).
+-define(LOGINFO(Text),           logger:info(Text, [], ?MFA_METADATA)).
+-define(LOGINFO(Text,Params),    logger:info(Text, Params, ?MFA_METADATA)).
+-define(LOGDEBUG(Text),          logger:debug(Text, [], ?MFA_METADATA)).
+-define(LOGDEBUG(Text,Params),   logger:debug(Text, Params, ?MFA_METADATA)).
+
+-else.
+
+-define(LOGERROR(Text),           ct:pal("error: " ++ Text)).
+-define(LOGERROR(Text, Params),   ct:pal("error: " ++ Text, Params)).
+-define(LOGWARNING(Text),         ct:pal("warning: " ++ Text)).
+-define(LOGWARNING(Text, Params), ct:pal("warning: " ++ Text, Params)).
+-define(LOGINFO(Text),            ct:pal("info: " ++ Text)).
+-define(LOGINFO(Text, Params),    ct:pal("info: " ++ Text, Params)).
+-define(LOGDEBUG(Text),           ct:pal("debug: " ++ Text)).
+-define(LOGDEBUG(Text, Params),   ct:pal("debug: " ++ Text, Params)).
+
+-endif.
 
 %% Structure Qualifier (SQ) types:
 %% 0 - Different IOAs
